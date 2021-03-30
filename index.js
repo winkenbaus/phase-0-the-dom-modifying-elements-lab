b3ec477efed6b75a84026244f4cfcc80585f0aaa
caitlin <caitlin.wink@gmail.com>
2021-03-30 12:13:24 -0700

    commit tree a74346d81a19411b1288be062c96d337df95bd32

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const removeMain = document.getElementById("main");
+removeMain.remove(removeMain.main);
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = '<h1>Victory</h1>';
+document.body.appendChild(newHeader)
+let text = document.createTextNode('Caitlin is the champion');
+newHeader.appendChild(text);
